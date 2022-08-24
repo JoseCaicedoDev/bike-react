24b224500bcdab0884bc8e7cc5a42aa373440fb7
jcaicedo <jose.caicedo@outlook.com>
2022-08-24 10:58:51 -0500

    [add] configurando theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>
-          Bike React colaborativo
+          Bike React colaborativo prueba
         </h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/themas.js
@@ -0,0 +1,10 @@
+/* import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap') */
+const theme = {
+  background: '#FAFAFA',
+  fontPrimary: '#1CA0F6',
+  fontSecundary: '#808080',
+  footer: '#222222',
+  fontFamily: '\'Roboto\', sanf-serif'
+}
+
+export default theme
