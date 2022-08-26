60b4580c511a2374111be99240431e9c22dcf0b1
jcaicedo <jose.caicedo@outlook.com>
2022-08-26 13:53:55 -0500

    [update] route

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import App from './App'
+import Home from './layouts/Home'
+import HomeClient from './layouts/HomeClient'
+import HomeUser from './layouts/HomeUser'
+import Status from './layouts/Status'
+import Login from './layouts/Login'
+import Register from './layouts/Register'
 
 const Index = () => {
   return (
@@ -12,6 +18,12 @@ const Index = () => {
         <Routes>
           {/* Rendering the App component when the path is '/'. */}
           <Route path='/' element={<App />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/homeUser' element={<HomeUser />} />
+          <Route path='/homeClient' element={<HomeClient />} />
+          <Route path='/status' element={<Status />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </Router>
     </>
