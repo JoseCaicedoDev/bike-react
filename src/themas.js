787956aa9faba8cacbfcf9b5761a89fcdb3f02c1
jcaicedo <jose.caicedo@outlook.com>
2022-09-13 17:42:51 -0500

    fix(colores)

--- src/themas.js
+++ src/themas.js
@@ -1,11 +1,4 @@
 const theme = {
-  background: '#233177',
-  backgroundGray: '#D9D9D9',
-  backgroundBlue: '#1A67BB',
-  backgroundBtn: '#1F86BF',
-  fontPrimary: 'rgb(255, 255, 255)',
-  fontFamily: '\'Inter\', sanf-serif',
-  transparencia: 'rgba(255,255,255,0.7)',
   backgroundColor: '#F1F2ED',
   primaryColor: '#6D848C',
   secondaryColor: '#444A58',
